b89c71dcd1d015a7238921476765c09c509bb3fd
Cid <147017642+Mit4ma@users.noreply.github.com>
2024-05-24 11:24:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    document.getElementById('display').value = 'I Miss Youu';
+}
